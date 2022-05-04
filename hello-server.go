6e5026b47da60b24d5c49a6af23444db77a9af5c
yousabu <shbanmhmdywsf8@gmail.com>
2022-05-04 12:01:16 +0200

    artifacts

---
+++ hello-server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+func startHttpServer(wg *sync.WaitGroup) *http.Server {
+	srv := &http.Server{Addr: ":11000"}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	})
+
+	go func() {
+		defer wg.Done() // let main know we are done cleaning up
+
+		// always returns error. ErrServerClosed on graceful close
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			// unexpected error. port in use?
+			log.Fatalf("ListenAndServe(): %v", err)
+		}
+	}()
+
+	// returning reference so caller can call Shutdown()
+	return srv
+}
+
+func main() {
+	log.Printf("main: starting HTTP server")
+
+	httpServerExitDone := &sync.WaitGroup{}
+
+	httpServerExitDone.Add(1)
+	srv := startHttpServer(httpServerExitDone)
+
+	log.Printf("main: serving for 10 seconds")
+
+	time.Sleep(10 * time.Second)
+
+	log.Printf("main: stopping HTTP server")
+
+	// now close the server gracefully ("shutdown")
+	// timeout could be given with a proper context
+	// (in real world you shouldn't use TODO()).
+	if err := srv.Shutdown(context.TODO()); err != nil {
+		panic(err) // failure/timeout shutting down the server gracefully
+	}
+
+	// wait for goroutine started in startHttpServer() to stop
+	httpServerExitDone.Wait()
+
+	log.Printf("main: done. exiting")
+}
